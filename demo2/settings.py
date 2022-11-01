7138de62f8a6606c79ab748fd626b404f8a53804
haru-sushiring <lemolemon00buroger@gmail.com>
2022-11-01 10:43:41 +0900

    static_root update

--- demo2/settings.py
+++ demo2/settings.py
@@ -152,4 +152,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ## error raise ImproperlyConfigured( django.core.exceptions.ImproperlyConfigured: You're using the staticfiles app without having set the STATIC_ROOT setting to a filesystem path.
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = ''
+# STATIC_ROOT = ''
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
