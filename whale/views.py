991f563dcd64e60149313c292234601a2bda7e16
haru-sushiring <lemolemon00buroger@gmail.com>
2022-11-15 11:15:37 +0900

    RegisterDBClass APIClass update

--- whale/views.py
+++ whale/views.py
@@ -38,73 +38,65 @@ def main(self):
 
       unix_timestamp = tsc.return_old_time_stamp()
       print('古いタイムスタンプを利用 : ' + str(unix_timestamp))
+      whale_api_response = api.return_whale_api(unix_timestamp)
 
-      time.sleep(15)
-      api_url = 'https://api.whale-alert.io/v1/transactions?'
-      payload = api.whale_api_param(unix_timestamp)
-      # r = requests.get(api_url, params=payload)
-      # new_btc_json = api.return_api_json(unix_timestamp)
-
-      error_flg = 0
-      while (error_flg == 0):
-          r = requests.get(api_url, params=payload)
-          # print(r.json())
-          match r.json():
-            case {"result": 'error', "message": error}:
-                print(r.json())
-                print(f"Response error!: {error}")
-                time.sleep(20)
+      error_flg = 1
+      while (error_flg == 1):
+        match whale_api_response.json():
+            case {"result": 'error', "message": error} if whale_api_response.status_code == 400:
+                print(f"timestamp error!: {error}") #value out of range for start parameter. For the Free plan the maximum transaction history is 3600 seconds
                 unix_timestamp = tsc.new_time_stamp()
-                payload = api.whale_api_param(unix_timestamp)
+                whale_api_response = api.return_whale_api(unix_timestamp)
+
+            case {"result": 'error', "message": error} if whale_api_response.status_code == 429:
+                print(f"requests error: {error}") #usage limit reached
+                time.sleep(20)
+                whale_api_response = api.return_whale_api(unix_timestamp)
+
             case {"result": 'success', "count": count} if count == 0:
-                time.sleep(15)
-                # print(r.json())
+                print('count : 0')
+                whale_api_response = api.return_whale_api(unix_timestamp)
+
             case {"result": 'success', "count": count} if count > 0:
-                error_flg = 1
+                error_flg = 0
                 # print('トランザクションが1個以上ある')
+
             case _:
                 print('不明なエラー jsonが取得できませんでした')
-
+                error_flg = 0
+                # whale_api_response = api.return_whale_api(unix_timestamp)
 
 
       print('ok')
-      new_btc_json = r.json()
-
-
-      btc_transactions_count = new_btc_json['count']
+      whale_api_json = whale_api_response.json()
+      btc_transactions_count = whale_api_json['count']
       print('count : ' + str(btc_transactions_count))
 
 
       # 同じタイムスタンプのトランザクションの値を処理する
-      comparison_time_stamp = 0
       sum_buy_btc_amount = 0
       sum_sell_btc_amount = 0
-      btc_jpy_price = 0
-      transactions_list = new_btc_json['transactions']
+      transactions_list = whale_api_json['transactions']
 
       # 同じタイムスタンプのトランザクションがある間、処理を行う
       for transaction in transactions_list:
         new_time_stamp = transaction['timestamp']
 
-        # 新しいタイムスタンプのトランザクションの時、初期化処理を行う
-        if (comparison_time_stamp == 0):
-            comparison_time_stamp = new_time_stamp
+        # 配列の要素が先頭の場合、初期化処理を行う
+        if (transaction == transactions_list[0]):
             tsc.register_time_stamp(new_time_stamp)
             timestamp = tsc.exchange_time_stamp(new_time_stamp) # タイムスタンプを日本時間に直す
-            btc_jpy_price = api.btc_jpy_price() # BTCの価格を取得する
+            btc_jpy_price = api.return_btc_jpy_price() # BTCの価格を取得する
 
-        # 新しく取得したトランザクションが前回のトランザクションのタイムスタンプと違う場合、breakして新しいトランザクションを取得する
-        if (comparison_time_stamp != new_time_stamp):
+        # 一つ前のタイムスタンプが、今配列から取り出したトランザクションのタイムスタンプと違う場合、db登録し、処理終了
+        if (tsc.return_old_time_stamp() != new_time_stamp):
             #1つ前のタイムスタンプを利用して新しいjsonデータを取得するために、関数に登録しておく
-            tsc.register_time_stamp(comparison_time_stamp)
-            # 同じタイムスタンプの時の、BTC移動の合計量とBTC価格をデータベースに登録する
-            if (sum_buy_btc_amount > 0):
-                move_side = 'buy'
-                rdbc.db_register(timestamp, sum_buy_btc_amount, btc_jpy_price, move_side)
-            if (sum_sell_btc_amount > 0):
-                move_side = 'sell'
-                rdbc.db_register(timestamp, sum_sell_btc_amount, btc_jpy_price, move_side)
+            # tsc.register_time_stamp(old_time_stamp)
+
+            # BTC移動の合計量とBTC価格をdbに登録する
+            rdbc.set_db(timestamp, btc_jpy_price, sum_buy_btc_amount, sum_sell_btc_amount)
             break
+
 
         btc_id = transaction['id']
         btc_from = transaction['from']['owner_type']
@@ -120,6 +112,11 @@ def main(self):
             alert_class.sell_alert(btc_amount)
             sum_sell_btc_amount += btc_amount
             print(sum_sell_btc_amount)
+
+        # 配列の要素が最後の場合（配列の中身がすべて同じタイムスタンプだった場合）db登録
+        if (transaction == transactions_list[-1]):
+            rdbc.set_db(timestamp, btc_jpy_price, sum_buy_btc_amount, sum_sell_btc_amount)
+            break
 
 
 ###
@@ -145,31 +142,62 @@ class TimeStampClass:
 
 ###
 class APIClass:
-    def whale_api_param(self, unix_timestamp):
+    def return_whale_api(self, unix_timestamp):
+        api_url = 'https://api.whale-alert.io/v1/transactions?'
         payload = {
             'api_key': os.environ['API_KEY'],
             'start': unix_timestamp,
             'currency': 'btc'
             }
 
-    #     time.sleep(15)
-    #     r = requests.get(api_url, params=payload)
+        time.sleep(15)
+        response = requests.get(api_url, params=payload)
 
-    #     # except r.JSONDecodeError:
-    #     #   print('JSONDecodeError')
-    #     #   return 'False'
+        error_flg = 1
+        while (error_flg == 1):
+            match response.status_code: #空の500,503エラーを避ける、400系エラー回避はメイン処理に組み込む
+                case 200:
+                    # print('whale-api success')
+                    error_flg = 0
 
-        return payload
+                case 500 | 503:
+                    print('500 503 error')
+                    time.sleep(10)
+                    response = requests.get(api_url)
+
+                case _:
+                    print('500 503 以外のerror')
+                    error_flg = 0
+
+        return response
 
 
-    def btc_jpy_price(self):
-        api_url = 'https://api.excelapi.org/crypto/rate?'
-        payload = {
-            'pair': 'btc-jpy'
-            }
+    def return_btc_jpy_price(self):
+        api_url = 'https://api.bitflyer.com/v1/getticker?product_code=BTC_JPY'
+        response = requests.get(api_url)
 
-        btc_jpy_price = requests.get(api_url, params=payload)
-        return btc_jpy_price.json()
+        error_flg = 1
+        while (error_flg == 1):
+            match response.status_code:
+                case 200:
+                    print('btc-jpy success')
+
+                    print('現在の価格' + str(response.json()['ltp']))
+                    error_flg = 0
+
+                case 500 | 503:
+                    print('500 503 error')
+                    time.sleep(10)
+                    response = requests.get(api_url)
+
+                case 400 | 401 | 403 | 404 | 408:
+                    print('400系 error')
+                    time.sleep(15)
+                    response = requests.get(api_url)
+                case _:
+                    print('不明なエラー btc-jpyを取得できませんでした')
+
+        return response.json()['ltp']
 
 
 ###
@@ -187,16 +215,17 @@ class RegisterDBClass:
     def __init__(self):
         self.DATABASES_URL = os.environ['DATABASE_URL']
 
-    def db_register(self, timestamp, btc_amount, btc_jpy_price, move_side):
-        amount = btc_amount
-        price = btc_jpy_price
-        move = move_side
+    def db_register(self, timestamp, amount, price, move):
         with psycopg2.connect(self.DATABASES_URL) as conn:
             with conn.cursor() as curs:
                 curs.execute(
                     "INSERT INTO whale(timestamp,amount,price,move) VALUES(timezone('JST' ,%s), %s, %s, %s)", (timestamp, amount, price, move))
 
         print('db登録しました ' + move)
+
+    def set_db(self, timestamp, btc_jpy_price, sum_buy_btc_amount, sum_sell_btc_amount):
+        self.db_register(timestamp, sum_buy_btc_amount, btc_jpy_price, 'buy')
+        self.db_register(timestamp, sum_sell_btc_amount, btc_jpy_price, 'sell')
 
 
 ###
